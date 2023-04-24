b49fcf70f5a4e410a0dc1a94ed5ba247605a6f59
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-24 21:50:40 +0800

    pseudocode

---
+++ file_handling-write_to_file.py
@@ -0,0 +1,10 @@
+# File handling - Writing multiple line of text contents into a text file
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# open mylife.txt file
+    # input line of text
+        # write line of text to mylife.txt
+        # ask user if there are more lines
+        # if yes, loop
+            # break loop
+# print "all files have been written"
