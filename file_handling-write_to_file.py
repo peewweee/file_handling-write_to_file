4b36b704c68d11e549f12003c0a81b7dd7233111
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-24 22:30:32 +0800

    added colors

--- file_handling-write_to_file.py
+++ file_handling-write_to_file.py
@@ -5,14 +5,14 @@
 with open("mylife.txt", "w") as text_file:
     # input line of text
     while True:
-        input_line = input("Enter a line you want to write: ")
+        input_line = input("\033[0;30;45m Enter a line you want to write: ")
         # write line of text to mylife.txt
         text_file.write(input_line + "\n")
         # ask user if there are more lines
-        ask_for_line = input("Do you want to add a line? (Yes/No): ")
+        ask_for_line = input("\033[0;36m Do you want to add a line? (Yes/No): ")
         # if yes, loop
         if ask_for_line != "Yes":
             # break loop
             break
 # print "all files have been written"
-print("All lines have been written. Thank you!")
+print("\033[0;35m All lines have been written. Thank you!")
