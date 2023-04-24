1e634a740be45e4afcefb65125d0aa9ae3f0a3e6
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-24 22:13:44 +0800

    loop is working

--- file_handling-write_to_file.py
+++ file_handling-write_to_file.py
@@ -4,10 +4,14 @@
 # open mylife.txt file
 with open("mylife.txt", "w") as text_file:
     # input line of text
-    input_line = input("Enter a line you want to write: ")
-    # write line of text to mylife.txt
-    text_file.write(input_line + "\n")
-    # ask user if there are more lines
-    # if yes, loop
-         # break loop
+    while True:
+        input_line = input("Enter a line you want to write: ")
+        # write line of text to mylife.txt
+        text_file.write(input_line + "\n")
+        # ask user if there are more lines
+        ask_for_line = input("Do you want to add a line? (Yes/No): ")
+        # if yes, loop
+        if ask_for_line != "Yes":
+            # break loop
+            break
 # print "all files have been written"
