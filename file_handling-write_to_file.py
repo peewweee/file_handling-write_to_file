72fd6f56e3bd37811e7759e33a31c00b438a6b4b
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-24 22:03:22 +0800

    .write code is working

--- file_handling-write_to_file.py
+++ file_handling-write_to_file.py
@@ -2,9 +2,12 @@
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
 # open mylife.txt file
+with open("mylife.txt", "w") as text_file:
     # input line of text
-        # write line of text to mylife.txt
-        # ask user if there are more lines
-        # if yes, loop
-            # break loop
+    input_line = input("Enter a line you want to write: ")
+    # write line of text to mylife.txt
+    text_file.write(input_line + "\n")
+    # ask user if there are more lines
+    # if yes, loop
+         # break loop
 # print "all files have been written"
